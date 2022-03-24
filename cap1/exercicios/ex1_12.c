b552e0dc7acc787dabdf8626ef3ccb91ab917d75
dbarlavento <14984401+dbarlavento@users.noreply.github.com>
2022-03-24 14:47:47 -0300

    adição do exercício 1.12

---
+++ cap1/exercicios/ex1_12.c
@@ -0,0 +1,26 @@
+/*
+ * Exercício 1.12 do Livro The C Programing Language 2 ed.
+ * Imprime a entrada, uma palavra por linha.
+ */
+
+ #include <stdio.h>
+
+ #define IN 1 //dentro de uma palvra
+ #define OUT 0 //fora da palavra
+
+ int main()
+ {
+ 	int c, //caracter de entrada
+		estado; //sinal, dentro ou fora de uma palvra
+
+		estado = OUT;
+
+		while (( c = getchar()) != EOF) { 
+			if( c == ' ' || c == '\n' || c == '\t')
+				putchar('\n');
+			else {
+				estado = IN;
+				putchar(c);
+			}
+		}//fim de while
+ }//fim de main
