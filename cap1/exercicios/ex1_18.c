0ed1cc38ab9dec710db33bc4a223a36eb0f38378
Daniel Gomes <daniel@EliteDesk.my.domain>
2022-05-10 09:49:44 -0300

    Limpeza no exercício 1.18. Agora só o necessário.

--- cap1/exercicios/ex1_18.c
+++ cap1/exercicios/ex1_18.c
@@ -10,21 +10,13 @@
 //Busca a proxima linha na entrada
 int getLine(char newline[], int length);
 
-//Copia uma matriz e a guarda en outro local
-void copyLine(char to[], char from[]);
-
 //Remove os espaços em branco e tabulações no fim de uma string.
 void remRigthBlank(char line[], int tam);
 
 int main()
 {
-	int len; //Comprimento máximo atual
-	int max; //Comprimento máximo já registrado
 	char line[MAXLINE]; //linha atual na entrada
-	char longest[MAXLINE]; //guarda a linha mais longa
 
-	len = max = 0;
-	
 	getLine(line, MAXLINE);
 	remRigthBlank(line, MAXLINE);
 	printf("%s", line);
@@ -42,7 +34,6 @@ int getLine(char line[], int tam)
 	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
 		if(i < tam - 1) {
 			line[i] = c;
-			//printf("char read = %c\n", c);
 		}
 	
 	if ((c == '\n') && (i < tam - 1)) {
@@ -77,20 +68,10 @@ void remRigthBlank(char line[], int tam)
 		if (line[i] == '\n' && line[i + 1] == '\0')
 			ff = i;
 
-		//printf("i = %d\t char = %c\n", i, line[i]); 
 		++i;
 	}
 
 	line[fc + 1] = '\n';
 	line[fc + 2] = '\0';
 
-//	if (ff) {
-//		line[fc + 1] = '\n';
-//		line[fc + 2] = '\0';
-//	}
-//	else
-//		line[fc + 1] = '\0';
-
-
-	//printf("FC = %d \t FF = %d \n", fc, ff);
 }
