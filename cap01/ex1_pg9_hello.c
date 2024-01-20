2f6c17dba2962f0683960c56f67626d35517ad7f
dbarlavento <14984401+dbarlavento@users.noreply.github.com>
2024-01-20 19:52:51 -0300

    Exemplo 1: Hello, world

---
+++ cap01/ex1_pg9_hello.c
@@ -0,0 +1,12 @@
+/*
+	Exemplo 1
+	Pagina 9
+	Imprime a mensagem "hello, world"
+*/
+
+#include <stdio.h>
+
+void main()
+{
+	printf("hello, world\n");
+}
--- cap1/exemplos/p1.c
+++
@@ -1,7 +0,0 @@
-/* Programa 1 do livro de K&R 2Ed. */
-#include <stdio.h>
-
-int main()
-{
-	printf("Hello, world!\n");
-}//fim de main
